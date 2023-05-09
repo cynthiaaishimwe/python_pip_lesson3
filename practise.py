1ed3ca7f4a0ae65e80247c878dd0d5948d641d6c
cyntiaaishimwe <ishimwecynthiaa@gmail.com>
2023-05-09 23:49:30 +0300

    assignment

---
+++ practise.py
@@ -0,0 +1,65 @@
+# Write a Python program that takes a list of numbers as input and 
+# outputs the second largest number in the list using
+# conditional statements and a for loop.
+
+def second_largest(num):
+      
+    second = sorted(num)
+    for n in second:
+        if len(second)<1:
+            return n
+        else:
+            new = max(second)
+            return (second[-2])
+          
+       
+num = [20,10,40,56,12,8,100]
+print(second_largest(num))
+
+
+
+# Write a Python program that takes a string as input and checks if it is a palindrome 
+# (reads the same forwards and backwards), 
+# ignoring spaces and punctuation.
+
+def palindrome(name):
+    if name== name[::-1]:
+        return True
+    else:
+        return False
+print(palindrome("dad"))
+
+# Write a Python program that takes a list of numbers 
+# as input and outputs the median of the numbers 
+
+def median(numbers):
+   sorted_list = sorted(numbers)
+   mid = len(sorted_list) // 2
+   if len(sorted_list) % 2 == 1:
+        return sorted_list[mid]
+   else:
+        return (sorted_list[mid- 1] + sorted_list[mid]) / 2
+   
+numbers = [20, 30, 90, 20, 3, 44, 50, 12]
+print(median(numbers))
+
+
+
+
+# Write a Python program that takes a list of strings as input and outputs the
+# number of times each string appears in the list, 
+# using a dictionary and conditional statements.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
